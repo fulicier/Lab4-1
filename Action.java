b39215a3f546111295c7b750781ac04e34031727
fulicier <1204449482@qq.com>
2014-10-15 18:11:27 +0800

    wrote a action file

---
+++ Action.java
@@ -0,0 +1,38 @@
+package ACT;
+import com.opensymphony.xwork2.ActionSupport;import Infor.Book;
+
+import Oper.Operate;
+import java.util.List;
+
+public class Action extends ActionSupport {
+	private List<Book> books;
+	private List<Book> authors;
+	private long isbn;
+	private String name;
+	private Book b;
+	public String List() throws Exception {
+		books = Operate.BookList(name);
+		//System.out.print("1");
+		return SUCCESS;
+	}
+	public String Delete(){
+		Operate.Delete(isbn);
+		return SUCCESS;
+	}
+	public String Detail() throws Exception {
+		b = Operate.Show(isbn);
+		return INPUT;
+	}
+	public String GetAuthor(){
+		return name;
+	}
+	public void SetAuthor(String name){
+		this.name = name;
+	}
+	public Long GetISBN(){
+		return isbn;
+	}
+	public void SetISBN(long isbn){
+		this.isbn = isbn;
+	}
+}
